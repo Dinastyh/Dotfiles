a8d333ba5f3eeadd6a60bd7c69b67054e0ba62b7
Paul Lege <paul.lege@epita.fr>
2023-02-24 09:12:55 +0100

    scripts: add configuration of gnupg

--- scripts/yubi_key.sh
+++ scripts/yubi_key.sh
@@ -2,6 +2,10 @@
 
 sudo pacman -S yubikey-personalization gnupg yubikey-manager-qt --noconfirm
 
+touch ~/.gnupg/gpg-agent.conf
+
+echo "enable-ssh-support" >> ~/.gnupg/gpg-agent.conf
+
 sudo echo 'export GPG_TTY="$(tty)"' | sudo tee -a /etc/profile
 sudo echo 'gpg-connect-agent updatestartuptty /bye' | sudo tee -a /etc/profile
 sudo echo 'export SSH_AUTH_SOCK="/run/user/$UID/gnupg/S.gpg-agent.ssh"' | sudo tee -a /etc/profile
