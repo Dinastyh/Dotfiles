80cddf7c673d14a16b4a52291bb9347918266f58
Paul LEGE <paul.lege@protonmail.com>
2024-11-29 12:03:41 +0100

    Update dotfile

--- NixOs/dinasty.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }:
-
-{
-	description = "Dinasty";	
-	isNormalUser = true;
-    createHome = true;
-    extraGroups = [ "wheel" "docker" "lxd" "vboxusers" ];
-    shell = pkgs.zsh;
-}
---
+++ install.zsh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+START_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+########
+
+echo "Setup Env"
+
+cd /etc/default
+sudo ln -s "$START_PATH/default/keyboard" .
+cd ~/
+ln -s "$START_PATH/basic/clang-format" .clang-format
+ln -s "$START_PATH/basic/vimrc" .vimrc
+
+exit
+
+########
+
+echo "Setup Config Files"
+
+mkdir -p ~/.config
+cd ~/.config
+
+ln -s "$START_PATH/i3/" .
+ln -s "$START_PATH/i3status/" .
+ln -s "$START_PATH/kitty/" .
+ln -s "$START_PATH/nvim/" .
+ln -s "$START_PATH/picom/" .
+ln -s "$START_PATH/zellij/" .
+
+cd ~/
+
+########
+
+echo "Setup Zsh"
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+cd ~/.oh-my-zsh/custom/plugins/
+ln -s "$START_PATH/zsh/plugins/zsh-autosuggestions" zsh-autosuggestions
+
+cd ~/
+rm -rf .zshrc
+ln -s "$START_PATH/zsh/zshrc" .zshrc
+
+########
